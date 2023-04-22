df22b160fee9edee8440b717d3f320a3398dc878
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-04-22 10:36:09 +0530

    backend connected

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { chats } = require("./data/data");
+const colors = require("colors");
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000
 
+dotenv.config();
+connectDB();
+
 app.get('/',(req,res)=>{
     res.send("API is Running")
+});
+
+app.get("/api/chat", (req,res) => {
+    res.send(chats);
 })
-app.listen(PORT,console.log(`Server started on PORT ${PORT}`));
+
+app.listen(PORT,console.log(`Server started on PORT ${PORT}`.yellow.bold));
