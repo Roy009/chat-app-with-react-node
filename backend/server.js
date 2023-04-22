1de87e949f3ce870aa6a3c979630aba22050d912
Rohit009 <63160366+Roy009@users.noreply.github.com>
2023-04-22 17:57:47 +0530

    routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const { chats } = require("./data/data");
 const colors = require("colors");
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 5000
 
@@ -13,8 +15,5 @@ app.get('/',(req,res)=>{
     res.send("API is Running")
 });
 
-app.get("/api/chat", (req,res) => {
-    res.send(chats);
-})
-
+app.use('/api/user', userRoutes)
 app.listen(PORT,console.log(`Server started on PORT ${PORT}`.yellow.bold));
